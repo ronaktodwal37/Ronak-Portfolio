14aedc7321eea67da9e869df19c00488a0341e03
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-18 23:17:07 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', () => {
     /* --- 2. Sticky Navbar & Active Link Highlight --- */
     // const navbar = document.getElementById('navbar');
     // const sections = document.querySelectorAll('section');
-    const navLinksArray = document.querySelectorAll('.nav-list .nav-link');
+    // const navLinksArray = document.querySelectorAll('.nav-list .nav-link');
 
     window.addEventListener('scroll', () => {
         // Sticky Navbar styling
